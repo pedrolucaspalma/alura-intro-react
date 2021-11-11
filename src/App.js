d80862aba8ef5102ee88815381201e1400a13196
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-11-11 14:12:35 -0300

    finish 3.2 3.3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from "react";
-import  ListaDeNotas  from "./components/ListaDeNotas";
+import  ListaDeNotas  from "./components/ListaDeNotas/ListaDeNotas";
 import  FormularioCadastro  from "./components/FormularioCadastro";
+import "./App.css"
 
 class  App extends Component {
   render(){
     return (
-      <section>
+      <section className="conteudo">
         <FormularioCadastro />
         <ListaDeNotas />
       </section>
--- src/components/FormularioCadastro.jsx
+++ src/components/FormularioCadastro.jsx
@@ -3,12 +3,21 @@ import React, { Component } from "react";
 class FormularioCadastro extends Component {
   render() {
     return (
-      <form>
-        <input type="text" placeholder="Título"></input>
-        <textarea placeholder="Escreva sua nota"></textarea>
-        <button>Criar nota</button>
+      <form className="form-cadastro">
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+        ></input>
+        <textarea
+          placeholder="Escreva sua nota"
+          className="form-cadastro_input"
+        ></textarea>
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar nota
+        </button>
       </form>
     );
   }
 }
-export default FormularioCadastro
+export default FormularioCadastro;
