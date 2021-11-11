db306eb72554bdf1d2350e5b9ca5f0d5f95b9657
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-11-11 13:47:32 -0300

    finish 2.5

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
-import CardNota from "./CardNota"
+import CardNota from "./CardNota";
 
 class ListaDeNotas extends Component {
   render() {
     return (
       <ul>
-        <li>
-            <CardNota></CardNota>
-        </li>
-        <li>
-            <CardNota></CardNota>
-        </li>
+        {Array.of("Trabalho,", "Trabalho", "Estudos").map((categoria) => {
+          return (
+            <li>
+              <div>{categoria}</div>
+              <CardNota></CardNota>
+            </li>
+          );
+        })}
       </ul>
     );
   }
 }
-export default ListaDeNotas
+export default ListaDeNotas;
