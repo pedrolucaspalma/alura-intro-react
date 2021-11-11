0dc519ffa5711790ec79d92c2849870136de4001
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-11-11 13:40:48 -0300

    finish 2.4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React from "react";
-import {ListaDeNotas} from "./components/ListaDeNotas"
+import React, {Component} from "react";
+import  ListaDeNotas  from "./components/ListaDeNotas";
+import  FormularioCadastro  from "./components/FormularioCadastro";
 
-function App() {
-  return (
-    <section>
-      <form>
-        <input type="text" placeholder="Título"></input>
-        <textarea placeholder="Escreva sua nota"></textarea>
-        <button>Criar nota</button>
-      </form>
-      <ListaDeNotas/>
-    </section>
-  );
+class  App extends Component {
+  render(){
+    return (
+      <section>
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,26 +1,18 @@
 import React, { Component } from "react";
+import CardNota from "./CardNota"
 
-export class ListaDeNotas extends Component {
+class ListaDeNotas extends Component {
   render() {
     return (
       <ul>
         <li>
-          <section>
-            <header>
-              <h3>Título</h3>
-            </header>
-            <p>Escreva sua nota</p>
-          </section>
+            <CardNota></CardNota>
         </li>
         <li>
-          <section>
-            <header>
-              <h3>Título</h3>
-            </header>
-            <p>Escreva sua nota</p>
-          </section>
+            <CardNota></CardNota>
         </li>
       </ul>
     );
   }
 }
+export default ListaDeNotas
